7cbadc09b5f9ce0ea1518bcc195f3be2f947b059
ZachPinfold <Zach@tibbi.co.uk>
2020-08-22 17:24:16 +0100

    swapped out the color actions for the new schemas

---
+++ src/actions/colors.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import {
+  GET_COLORS,
+  COLOR_ERROR,
+  GET_RANDOM_COLORS,
+  GET_SECOND_COLORS
+} from "./types";
+import { API, graphqlOperation } from "aws-amplify";
+import { listColorHexs } from "../graphql/queries";
+
+// load all colors
+
+export const getColors = (random, first) => async dispatch => {
+  try {
+    const runAPI = async () => {
+      console.log("runAPI");
+      const result1 = await API.graphql(
+        graphqlOperation(listColorHexs, { format: "json" })
+      );
+      const obj = JSON.parse(result1.data.listColorHexs);
+      if (obj[0].colors.length === 5) return obj[0].colors;
+      else return runAPI();
+    };
+
+    const APIResult = runAPI();
+
+    APIResult.then(payload => {
+      dispatch({
+        type: random === "new_random" ? GET_RANDOM_COLORS : GET_COLORS,
+        payload: payload
+      });
+    });
+  } catch (error) {
+    dispatch({
+      type: COLOR_ERROR
+    });
+  }
+
+  if (first === true) {
+    try {
+      const runAPI = async () => {
+        console.log("runAPI");
+        const result1 = await API.graphql(
+          graphqlOperation(listColorHexs, { format: "json" })
+        );
+        const obj = JSON.parse(result1.data.listColorHexs);
+        if (obj[0].colors.length === 5) return obj[0].colors;
+        else return runAPI();
+      };
+
+      const APIResult = runAPI();
+
+      APIResult.then(payload => {
+        dispatch({
+          type: GET_SECOND_COLORS,
+          payload: payload
+        });
+      });
+    } catch (error) {
+      dispatch({
+        type: COLOR_ERROR
+      });
+    }
+  }
+};
--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -2,21 +2,10 @@ import React, { Component } from "react";
 import Backgroud from "./Background";
 import { API, graphqlOperation } from "aws-amplify";
 import { listColorHexs } from "../../graphql/queries";
-import axios from "axios";
 
 class Landing extends Component {
   componentDidMount = () => {
     this.getPosts();
-
-    // axios
-    //   .all([axios.get(`api/palette`), axios.get(`/api/breeds/image/random`)])
-    //   .then(
-    //     axios.spread((user, dog) => {
-    //       const users = user.data;
-    //       const dogs = dog.data;
-    //       console.log(users, dogs);
-    //     })
-    //   );
   };
 
   getPosts = async () => {
@@ -25,7 +14,7 @@ class Landing extends Component {
     );
     // console.log(result.data);
     const obj = JSON.parse(result.data.listColorHexs);
-    console.log(obj[0]);
+    // console.log(obj[0].colors);
   };
 
   render() {
